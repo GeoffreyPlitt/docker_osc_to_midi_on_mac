9920507b70e8221b285524c26cc8faaf105e2e46
Geoffrey Plitt <geoff@gweb.org>
2025-08-05 20:06:23 -0700

    clean go.sh

--- go.sh
+++ go.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-# Interactive launcher script
-# First select MIDI device, then run honcho
-
 set -e
 
-echo "=== OSC2MIDI Bridge Setup ==="
-echo "First, let's select your MIDI device..."
-
-# Run MIDI device selection interactively and source the exports
 source ./find_midi_devices.sh
 
-echo ""
-echo "Now starting the complete system..."
-echo "Press Ctrl+C to stop everything"
-echo ""
-
-# Start honcho with the system
 exec honcho start
